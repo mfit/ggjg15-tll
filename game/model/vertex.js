416392d68fa006c55f79b85dd55b59bcea2674fd
Christian Paier <c.paier@student.tugraz.at>
2015-01-23 23:15:29 +0100

    starting the graph

---
+++ game/model/vertex.js
@@ -0,0 +1,20 @@
+'use strict';
+function Vertex(name, data) {
+  this.init(name, data);
+}
+   
+
+Vertex.prototype = {
+  init: function(name, data) {
+    this._name  = name;
+    this._data  = data;
+    this._edges = [];
+  },
+
+  addEdges: function(edges) {
+    this._edges.concat(edges);
+  },
+}
+
+
+module.exports = Vertex;
