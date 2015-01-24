bf58730f3e4f7143d258992a4ee2e8d6ff993d99
Christian Paier <c.paier@student.tugraz.at>
2015-01-24 12:21:01 +0100

    basic interaction

--- game/model/gamemaster.js
+++ game/model/gamemaster.js
@@ -1,11 +1,15 @@
 'use strict';
-function GameMaster(maxRound) {
-  this.init(maxRound);
+
+var Graph = require("../model/graph");
+
+function GameMaster(graph, maxRound) {
+  this.init(graph, maxRound);
 }
 
 GameMaster.prototype = {
-  init: function(maxRound) {
-      this._tick = 0;
+  init: function(graph, maxRound) {
+      this._tick     = 0;
+      this._graph    = graph;
       this._maxRound = maxRound;
   },
 
@@ -13,24 +17,65 @@ GameMaster.prototype = {
       this._tick += 1;
   },
 
-  playRound: function() {
+  playRound: function(interactions, propertyChangeCallback) {
       this.tick();
       console.log("current round " + this._tick);
-      // TODO
       if (this._tick >= this._maxRound) {
           this.finalStep();
+          return;
+      }
+
+      if (interactions === undefined) {
+          console.error("interactions are empty - but not last round");
+          return;
+      }
+
+      for (var index = 0; index < interactions.length; index++) {
+          if (interactions[index] === undefined) {
+              continue;
+          }
+
+          this.interact(interactions[index].from,
+                        interactions[index].to,
+                        interactions[index].topic,
+                        interactions[index].props,
+                        propertyChangeCallback);
       }
   },
 
   finalStep: function() {
       console.log("finalStep");
   },
+
+  interact: function(from, to, topic, props, callback) {
+
+      return callback(this._graph._vertices[from],
+                      this._graph._vertices[to], this._tick, topic, props);
+
+  },
+
 }
 
 
 function test() {
-  var gm = new GameMaster(3);
-  gm.playRound();
+  var g = new Graph();
+  g.addVertex('Susi',  {data: 5});
+  g.addVertex('Fritz', {data: 10});
+  g.addVertex('Sarah', {data: 15});
+  g.addVertex('HAL9000');
+  g.addEdge("Susi",  "Sarah" , {influence: 5});
+  g.addEdge("Susi",  "Fritz" , {influence: 10});
+  g.addEdge("Fritz", "Susi"  , {influence: 20});
+  g.addEdge("Fritz", "Sarah" , {influence: 10});
+  g.addEdge("HAL9000", "Fritz");
+
+
+  var interactions1 = [
+    {from: "Susi", to: "Sarah", topic: "blablabla", props: {}},
+  ];
+
+  var gm = new GameMaster(g, 3);
+  gm.playRound(interactions1);
   gm.playRound();
   gm.playRound();
 }
